d5b812fce84d70a67387a9c475b5ac1b708dfb56
Krzysztof Kedziorski <krzysztof.kedziorski@e-qsg.com>
2018-06-18 19:07:26 +0200

    empty project generated from start.spring.io

---
+++ src/main/java/pl/kedziorski/tools/db/ErDiagramEndpointApplication.java
@@ -0,0 +1,12 @@
+package pl.kedziorski.tools.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErDiagramEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErDiagramEndpointApplication.class, args);
+	}
+}
